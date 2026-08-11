5b8ac070845de2fc6bfe4fc6324682ddadc85f16
Urazovskyi Oleksandr <urazovskyi.sasha@gmail.com>
2026-08-11 20:31:50 +0300

    welcome page was added as a default view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -5,6 +6,7 @@ namespace RecruitingPlatform.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
