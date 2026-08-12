00ceb443dd9dcf5c449f4fce6b37cc1e193f63be
Urazovskyi Oleksandr <urazovskyi.sasha@gmail.com>
2026-08-12 15:54:57 +0300

    log in and sign up pages were added

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecruitingPlatform.Controllers;
+
+public class AuthController : Controller
+{
+    [HttpGet]
+    public IActionResult LogIn()
+    {
+        return View();
+    }
+    
+    [HttpGet]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+}
