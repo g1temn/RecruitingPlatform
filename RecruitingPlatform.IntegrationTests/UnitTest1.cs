0c2e0e1be26bae76b5276b2cbf414272f0f9b3e9
Urazovskyi Oleksandr <urazovskyi.sasha@gmail.com>
2026-08-30 23:59:54 +0300

    Refactor: Move projects into a single solution structure

---
+++ RecruitingPlatform.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace RecruitingPlatform.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ RecruitingPlatform.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace RecruitingPlatform.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
